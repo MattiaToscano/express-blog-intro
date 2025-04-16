9bc5702d18673d4413b625bacc00bb679ae3dc97
MattiaToscano <mattia1997ct@gmail.com>
2025-04-16 14:05:24 +0200

    [UPD] Creata cartella public e inserita cartella img dentro essa

--- app.js
+++ app.js
@@ -7,46 +7,45 @@ const app = express();
 //Definisco la porta
 const port = 3000;
 
+// Configuro gli asset statici
+app.use(express.static("public")); 
+
 //Creo rotta
 app.get("/", (req,res) =>{
     res.send("Server del mio Blog");
 })
 
-
+//creo altra rotta
 app.get("/bacheca", (req,res) =>{
     const posts = [
         {       titolo: "Primo Post", 
                 contenuto: "Contenuto del primo post",
-                immagine:"<img src= 'img/ciambellone.jpeg' alt='ciambellone'/>",
+                immagine: "<images/ciambellone.jpeg>",
                 tags: [`tag1`, `tag2`, `tag3`]
         },
         {       titolo: "Secondo Post", 
                 contenuto: "Contenuto del secondo post",
-                immagine:"img/post2.jpg",
-                tags: [`tag1`, `tag2`, `tag3`],
+                immagine: "<images/cracker_barbabietola.jpeg>",
+                tags: [`tag1`, `tag2`, `tag3`]
         },
         {       titolo: "Terzo Post", 
                 contenuto: "Contenuto del terzo post",
-                immagine:"",
+                immagine: "<images/pane_fritto_dolce.jpeg>",
                 tags: [`tag1`, `tag2`, `tag3`]
         },
         {       titolo: "Quarto Post", 
                 contenuto: "Contenuto del quarto post",
-                immagine:"",
+                immagine: "<images/pasta_barbabietola.jpeg>",
                 tags: [`tag1`, `tag2`, `tag3`]
         },
         {       titolo: "Quinto Post", 
                 contenuto: "Contenuto del quinto post",
-                immagine:"",
+                immagine: "<images/torta_paesana.jpeg>",
                 tags: [`tag1`, `tag2`, `tag3`]
         },
     ];
     res.json(posts);
 });
-
-
-
-
 
 app.listen(port,() => {
     console.log(`Server in ascolto sulla porta ${port}`);
