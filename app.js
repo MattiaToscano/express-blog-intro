665118b0d2f50781524465dfca9f5bf3b75f5b99
MattiaToscano <mattia1997ct@gmail.com>
2025-04-16 13:35:03 +0200

    [UPD] Importato express, assegnato, definita porta e creata rotta

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+//Importo express 
+const express = require('express');
+
+//Assegno express a una variabile
+const app = express();
+
+//Definisco la porta
+const port = 3000;
+
+//Creo rotta
+app.get("/", (req,res) =>{
+    res.send("Server del mio Blog");
+})
